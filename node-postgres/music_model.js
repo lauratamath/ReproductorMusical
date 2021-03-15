cbafce84b87adf95884534b70b4bd7aeb1344501
Andrea Amaya <andreaa.malin@gmail.com>
2021-03-15 17:50:12 -0600

    Manager para 3 rep en free

--- node-postgres/music_model.js
+++ node-postgres/music_model.js
@@ -18,9 +18,9 @@ const getUsersAccounts = () => {
   }) 
 }
 
-const getSongs = () => {
+const getFreeMembership = () => {
   return new Promise(function(resolve, reject) {
-    pool.query('SELECT * FROM songs', (error, results) => {
+    pool.query('SELECT * FROM freemembership', (error, results) => {
       if (error) {
         reject(error)
       }
@@ -37,13 +37,96 @@ const createUserAccount = (body) => {
         reject(error)
         console.log(error)
       }
-      resolve('A new account has been added added', results)
+      resolve('A new account has been added', results)
     })
   })
 }
-  
+
+const getAccountManager = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM accountmanager', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+  })
+}) 
+}
+
+const updateAccountManager = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { actualTrack, actualUsername, songName } = body
+    
+    pool.query("UPDATE accountmanager SET tracks = " + actualTrack + " WHERE username = '"+ actualUsername +"' AND song = '" + songName + "'", (error, results) => {
+      
+      if (error) {
+        reject(error)
+      }
+      resolve('Account Manager has been updated')
+  })
+}) 
+}
+
+const createAccountManager = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { actualUsername, songName, songArtist, actualDate, actualTrack } = body
+    pool.query('INSERT INTO accountmanager VALUES ($1, $2, $3, $4, $5)', [actualUsername, songName, songArtist, actualDate, actualTrack], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve('A new account manager has been added', results)
+    })
+  })
+}
+
+const getSongs = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM songs', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+  })
+}) 
+}
+
+const updateFreeMembershipDay = (body) => {
+  console.log(body)
+  return new Promise(function(resolve, reject) {
+    const { actualUsername, actualDateFree, actualTrackFree } = body
+    
+    pool.query("UPDATE freemembership SET tracks = " + actualTrackFree + " WHERE username = '"+ actualUsername +"' AND dateTime = '" + actualDateFree + "'", (error, results) => {
+      
+      if (error) {
+        reject(error)
+      }
+      resolve('Free Membership has been updated')
+  })
+}) 
+}
+
+const createFreeMembershipDay = (body) => {
+  console.log(body)
+  return new Promise(function(resolve, reject) {
+    const { actualUsername, actualDateFree, actualTrackFree } = body
+    pool.query('INSERT INTO freemembership VALUES ($1, $2, $3)', [actualUsername, actualDateFree, actualTrackFree], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve('Free membership has been added', results)
+    })
+  })
+}
+
+
 module.exports = {
   getUsersAccounts,
   createUserAccount,
-  getSongs
+  getSongs,
+  getAccountManager,
+  updateAccountManager,
+  createAccountManager,
+  updateFreeMembershipDay,
+  createFreeMembershipDay,
+  getFreeMembership
 }
--- react-postgres/src/components/memberships/functions/PlaySong.js
+++ react-postgres/src/components/memberships/functions/PlaySong.js
@@ -11,45 +11,141 @@ export const Play = ({onClick}) => {
     return <button style={styleButton} onClick={onClick}>Play</button>
 }
 
+const actualDate = () => {
+    const today = new Date()
+    const month = today.getMonth()+1
+
+    if (month.toString().length === 1){
+        return today.getFullYear() + '-0' + (today.getMonth() + 1) + '-' + today.getDate();
+    } else {
+        return today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    }
+}
+
 const actualUsername = localStorage.getItem('actualUsername')
-const PlaySong = () => {
+const date = actualDate()
+
+const PlaySong = ({songName, songArtist}) => {
     const [accountManager, setAccountManager] = useState([])
-    const [actualTrack, setActualTrack] = useState([])
+    const [freeMembershipManager, setFreeMembershipManager] = useState([])
   
     useEffect(() => {
-      getAccountManager();
-      setTracks();
+        getAccountManager();
     }, []);
-  
+
+    useEffect(() => {
+        getFreeMembership();
+    }, []);
+
     function getAccountManager() {
         fetch('http://localhost:3001/accountManager')
           .then(r => r.json())
           .then(r => setAccountManager(r))
     }
 
-    function setTracks () {
-        for (var a=0; a<accountManager.length; a++) {
-            if (accountManager[a].username == actualUsername){
-                setActualTrack(accountManager[a].tracks)
+    function getFreeMembership() {
+        fetch('http://localhost:3001/freemembership')
+          .then(r => r.json())
+          .then(r => setFreeMembershipManager(r))
+    }
+
+    function updateAccountManager() {
+        const [actualDate, actualTrack] = getTracks()
+        const [actualDateFree, actualTrackFree] = getTracksFree()
+
+        if (actualTrackFree<4 || actualTrackFree == undefined){ //Si no ha escuchado +3 o es usuario premium
+            //LO AGREGAMOS A LA TABLA DE ACCOUNTMANAGER
+            if (actualTrack !== 1){
+                fetch('http://localhost:3001/accountManager/', { 
+                    method: 'PUT',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({actualTrack, actualUsername, songName}),
+                    }).then(response => {
+                        return response.text();
+                    }).then(data => {
+                        alert(data);
+                        getAccountManager()
+                    });
+            } else {
+                fetch('http://localhost:3001/accountManager/', { 
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({actualTrack, actualUsername, songName, songArtist, actualDate}),
+                    }).then(response => {
+                        return response.text();
+                    }).then(data => {
+                        alert(data);
+                        getAccountManager()
+                    });      
+            }
+            //SI ES FREE, LE HACEMOS UPDATE A LA TABLA DE FREEMEMBERSHIP
+            console.log(actualUsername + ' ' + actualDateFree + ' ' + actualTrackFree)
+            if (actualTrackFree !== 1){
+                fetch('http://localhost:3001/freemembership', { 
+                    method: 'PUT',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({actualUsername, actualDateFree, actualTrackFree}),
+                    }).then(response => {
+                        return response.text();
+                    }).then(data => {
+                        alert(data);
+                        getFreeMembership()
+                    });
+            } else {
+                fetch('http://localhost:3001/freemembership', { 
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({actualUsername, actualDateFree, actualTrackFree}),
+                    }).then(response => {
+                        return response.text();
+                    }).then(data => {
+                        alert(data);
+                        getFreeMembership()
+                    });      
+            }           
+        }
+    }
+
+    function getTracks () {
+        for (var a=0; a<accountManager.length; a++) { 
+            const dbDate = accountManager[a].datetime.substring(0,10)
+            if (accountManager[a].username === actualUsername && accountManager[a].song === songName && dbDate === date){
+                accountManager[a].tracks += 1
+                return [date, accountManager[a].tracks]
+            } else if (accountManager[a].username === actualUsername){
+                return [date, 1] //Es primera vez que escucha la cancion
             }
         }
     }
-    console.log('render', accountManager)
 
-    const handleChange = (event) => {
-        fetch('http://localhost:3001/accountManager', {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({actualTrack, actualUsername}),
-            }).then(response => {
-              return response.text();
-            });
+    function getTracksFree () {
+        for (var a=0; a<freeMembershipManager.length; a++) { 
+            const dbDate = freeMembershipManager[a].datetime.substring(0,10)
+            if (freeMembershipManager[a].username === actualUsername && dbDate === date){
+                freeMembershipManager[a].tracks += 1 
+                return [date, freeMembershipManager[a].tracks]
+            } 
+        }
+        for (var a=0; a<freeMembershipManager.length; a++) { 
+            if (freeMembershipManager[a].username === actualUsername){
+                return [date, 1] //Es primera vez que escucha en el dia
+            }
+        }
+        return [undefined, undefined]
     }
+    
+    console.log('render', freeMembershipManager)
 
     return (
-        <Play onClick={handleChange}/>
+        <Play onClick={updateAccountManager}/>
     )
 }
 export default PlaySong;
