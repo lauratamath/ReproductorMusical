fbb43be4fe8993c4adf2adccbbdaf2ebebec5c5f
lauratamath <laurylevi@hotmail.com>
2021-05-11 20:39:09 -0600

    arreglo de botones de reports

--- frontend/src/components/memberships/functions/GetReports.js
+++ frontend/src/components/memberships/functions/GetReports.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import './FreeAccount.css';
+import './reports/reports.css'
 
 const Button = ({onClick, text}) => {
     const style = {
     color: '#FFFFFF'
   }
-  return <button onClick={onClick} style={style} className = 'otross'>{text}</button>
+  return <button onClick={onClick} style={style} className = 'otrossR'>{text}</button>
 }
 
 const GetReports = () => {
@@ -32,21 +33,20 @@ const GetReports = () => {
                 Get reports
             </font>
         </h1>
+        <br/><br/><br/>
         <center>
-        <br/><br/><br/><br/><br/>
-            <Button onClick={() => history.push('./reports/songsCount')} text='Artistas con mayor producción'/>
-            <Button onClick={() => history.push('./reports/popularArtists')} text='Artistas con popularidad creciente'/>
-            <Button onClick={() => history.push('./reports/subscriptionCount')} text='Cantidad de nuevas suscripciones'/>
-            <br/><br/><br/>
-            <Button onClick={() => history.push('./reports/albumReleases')} text='Álbumes más recientes'/>
-            <Button onClick={() => history.push('./reports/popularGenders')} text='Generos más populares'/>
-            <Button onClick={() => history.push('./reports/activeUsers')} text='Usuarios más activos'/>
-            <br/><br/><br/>
-            <Button onClick={() => history.push('./reports/salesPerWeek')} text='Ventas por Semana'/>
-            <Button onClick={() => history.push('./reports/mostSelledArtist')} text='Artistas con Mayores Ventas'/>
-            <Button onClick={() => history.push('./reports/salesPerGenre')} text='Ventas por Género'/>
-            <br/><br/><br/>
-            <Button onClick={() => history.push('./reports/topSongs')} text='Canciones más reproducidas'/>
+            <div className="filaR">
+                <Button onClick={() => history.push('./reports/songsCount')} text='Artistas con mayor producción'/>
+                <Button onClick={() => history.push('./reports/popularArtists')} text='Artistas con popularidad creciente'/>
+                <Button onClick={() => history.push('./reports/subscriptionCount')} text='Cantidad de nuevas suscripciones'/>
+                <Button onClick={() => history.push('./reports/albumReleases')} text='Álbumes más recientes'/>
+                <Button onClick={() => history.push('./reports/popularGenders')} text='Generos más populares'/>
+                <Button onClick={() => history.push('./reports/activeUsers')} text='Usuarios más activos'/>
+                <Button onClick={() => history.push('./reports/salesPerWeek')} text='Ventas por Semana'/>
+                <Button onClick={() => history.push('./reports/mostSelledArtist')} text='Artistas con Mayores Ventas'/>
+                <Button onClick={() => history.push('./reports/salesPerGenre')} text='Ventas por Género'/>
+                <Button onClick={() => history.push('./reports/topSongs')} text='Canciones más reproducidas'/>
+            </div>
         </center>
     </div>
   );
